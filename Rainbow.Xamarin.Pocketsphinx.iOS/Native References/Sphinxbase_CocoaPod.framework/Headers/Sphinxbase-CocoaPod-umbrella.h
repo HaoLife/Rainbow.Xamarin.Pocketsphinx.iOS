724574aa17bc746ab8ecba8fe7cc8f70370a7e5e
hao.liu <hao.liu@weimob.com>
2019-04-26 14:24:13 +0800

    修改

---
+++ Rainbow.Xamarin.Pocketsphinx.iOS/Native References/Sphinxbase_CocoaPod.framework/Headers/Sphinxbase-CocoaPod-umbrella.h
@@ -0,0 +1,53 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "ad.h"
+#import "agc.h"
+#import "bio.h"
+#import "bitarr.h"
+#import "bitvec.h"
+#import "byteorder.h"
+#import "case.h"
+#import "ckd_alloc.h"
+#import "clapack_lite.h"
+#import "cmd_ln.h"
+#import "cmn.h"
+#import "err.h"
+#import "f2c.h"
+#import "fe.h"
+#import "feat.h"
+#import "filename.h"
+#import "fixpoint.h"
+#import "fsg_model.h"
+#import "genrand.h"
+#import "glist.h"
+#import "hash_table.h"
+#import "heap.h"
+#import "jsgf.h"
+#import "listelem_alloc.h"
+#import "logmath.h"
+#import "matrix.h"
+#import "mmio.h"
+#import "ngram_model.h"
+#import "pio.h"
+#import "prim_type.h"
+#import "priority_queue.h"
+#import "profile.h"
+#import "sbthread.h"
+#import "sphinxbase_export.h"
+#import "sphinx_config.h"
+#import "strfuncs.h"
+#import "yin.h"
+
+FOUNDATION_EXPORT double Sphinxbase_CocoaPodVersionNumber;
+FOUNDATION_EXPORT const unsigned char Sphinxbase_CocoaPodVersionString[];
+
--- Rainbow.Xamarin.Pocketsphinx.iOS/obj/Debug/LinkWithAttributes.cs
+++
@@ -1,1 +0,0 @@
-[assembly: ObjCRuntime.LinkWith ("Sphinxbase_CocoaPod.framework")]
