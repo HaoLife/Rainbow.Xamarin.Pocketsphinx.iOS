f6b250101fbd754538118010f10f6bbcfee6869d
hao.liu <hao.liu@weimob.com>
2019-04-26 16:23:13 +0800

    修改

--- Rainbow.Xamarin.Pocketsphinx.iOS/Sphinxbase_CocoaPod_ApiDefinitions.cs
+++ Rainbow.Xamarin.Pocketsphinx.iOS/Sphinxbase_CocoaPod_ApiDefinitions.cs
@@ -1,23 +1,23 @@
-using Foundation;
+//using Foundation;
 
 
-partial interface Constants
-{
-	// extern const char *[] agc_type_str;
-	[Field ("agc_type_str", "__Internal")]
-	unsafe sbyte*[] agc_type_str { get; }
+//partial interface Constants
+//{
+//    // extern const char *[] agc_type_str;
+//    [Field("agc_type_str", "__Internal")]
+//    unsafe sbyte*[] agc_type_str { get; }
 
 
-    // extern const char *[] cmn_type_str;
-    [Field("cmn_type_str", "__Internal")]
-    unsafe sbyte*[] cmn_type_str { get; }
+//    // extern const char *[] cmn_type_str;
+//    [Field("cmn_type_str", "__Internal")]
+//    unsafe sbyte*[] cmn_type_str { get; }
 
 
-    // extern double Sphinxbase_CocoaPodVersionNumber;
-    [Field("Sphinxbase_CocoaPodVersionNumber", "__Internal")]
-    double Sphinxbase_CocoaPodVersionNumber { get; }
+//    // extern double Sphinxbase_CocoaPodVersionNumber;
+//    [Field("Sphinxbase_CocoaPodVersionNumber", "__Internal")]
+//    double Sphinxbase_CocoaPodVersionNumber { get; }
 
-    // extern const unsigned char [] Sphinxbase_CocoaPodVersionString;
-    [Field("Sphinxbase_CocoaPodVersionString", "__Internal")]
-    byte[] Sphinxbase_CocoaPodVersionString { get; }
-}
+//    // extern const unsigned char [] Sphinxbase_CocoaPodVersionString;
+//    [Field("Sphinxbase_CocoaPodVersionString", "__Internal")]
+//    byte[] Sphinxbase_CocoaPodVersionString { get; }
+//}
--- Rainbow.Xamarin.Pocketsphinx.iOS/Structs.cs
+++ Rainbow.Xamarin.Pocketsphinx.iOS/Structs.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿//using System;
 
-namespace NativeLibrary
-{
-}
+//namespace NativeLibrary
+//{
+//}
 
